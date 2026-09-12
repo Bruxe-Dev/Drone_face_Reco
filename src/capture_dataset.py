7cad360f4afc824a9e65c68551ef019b4c8d6d50
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-12 20:31:59 +0200

    Added an image enhancment function

--- src/capture_dataset.py
+++ src/capture_dataset.py
@@ -2,6 +2,22 @@ import cv2
 import os
 import time
 
+def enhance_frame(frame):
+    lab = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
+
+    l, a, b = cv2.split(lab)
+
+    clahe = cv2.createCLAHE(
+        clipLimit=2.0,
+        tileGridSize=(8, 8)
+    )
+
+    l = clahe.apply(l)
+
+    enhanced = cv2.merge((l, a, b))
+
+    return cv2.cvtColor(enhanced, cv2.COLOR_LAB2BGR)
+    
 name = input("Enter person's name: ")
 
 output_folder = os.path.join("data", name)
