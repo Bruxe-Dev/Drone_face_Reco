8a8f5b094b946b4e71477afce6d360ca268c7ee4
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-13 12:53:18 +0200

    Updated the main.py file

--- src/main.py
+++ src/main.py
@@ -1,22 +1,45 @@
 import cv2
-from detection import detect_faces
+from recognition import recognize_face
 
 
-camera = cv2.VideoCapture(1)
+camera = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+
+if not camera.isOpened():
+    print("Could not access camera")
+    exit()
+
+
+print("Camera started!")
+print("Press Q to quit.")
+
 
 while True:
+
     success, frame = camera.read()
 
     if not success:
-        print("Camera Access Failed!")
+        print("Could not read frame")
         break
 
     frame = cv2.flip(frame, 1)
 
-    faces = detect_faces(frame)
+    height, width = frame.shape[:2]
+
+    from detection import detector
+
+    detector.setInputSize((width, height))
+
+    _, faces = detector.detect(frame)
 
     if faces is not None:
+
         for face in faces:
+
+            name, score = recognize_face(
+                frame,
+                face
+            )
+
             x, y, w, h = face[:4]
 
             x = int(x)
@@ -28,11 +51,28 @@ while True:
                 frame,
                 (x, y),
                 (x + w, y + h),
-                (255, 0, 0),
+                (0, 255, 0),
+                2
+            )
+            label = f"{name} ({score:.2f})"
+
+            cv2.putText(
+                frame,
+                label,
+                (x, y - 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.7,
+                (0, 255, 0),
                 2
             )
 
-    cv2.imshow("DFR", frame)
+
+
+    cv2.imshow(
+        "Drone Face Recognition",
+        frame
+    )
+
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
