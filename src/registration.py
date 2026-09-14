f1b1b63ee4fb20d306afcd1d247ba6b22003afbe
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-14 19:45:36 +0200

    Finished up the registration script

--- src/registration.py
+++ src/registration.py
@@ -4,6 +4,8 @@ import time
 from recognition import get_embedding
 from vector_store import add_vectors
 
+MODEL_PATH = "models/face_detection_yunet_2023mr.onnx"
+
 def register_person(cap,name,number_of_images=3):
     embedding = []
 
@@ -33,3 +35,54 @@ def register_person(cap,name,number_of_images=3):
         cv2.imshow(
             "Face Recognition",frame
         )
+
+        detector = cv2.FaceDetectorYN.create(
+            MODEL_PATH,
+            "",
+            (320,320),
+            0.9,
+            0.3,
+            5000
+        )
+
+        height, width = frame.shape[:2]
+        detector.setInputSize((width,height))
+
+        _,faces = detector.detect(frame)
+
+        if faces is not None and len(faces) > 0:
+
+            # Use the largest detected face
+            face = max(
+                faces,
+                key=lambda f: f[2] * f[3]
+            )
+
+            embedding = get_embedding(frame, face)
+
+            embeddings.append(embedding)
+
+            print(
+                f"Captured sample "
+                f"{len(embeddings)}/{number_of_images}"
+            )
+
+            time.sleep(1)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    cv2.destroyWindow("Face Registration")
+
+    if len(embeddings) == number_of_images:
+
+        for embedding in embeddings:
+            add_face(name, embedding)
+
+        print(f"\n{name} successfully registered!")
+
+        return True
+
+    print("\nRegistration cancelled.")
+
+    return False
