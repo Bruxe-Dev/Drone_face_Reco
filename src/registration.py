ec144a040c9811ddcfa5b43b6b76ee352a596aa3
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-14 19:16:51 +0200

    Made the face recognition frame input

--- src/registration.py
+++ src/registration.py
@@ -5,4 +5,31 @@ from recognition import get_embedding
 from vector_store import add_vectors
 
 def register_person(cap,name,number_of_images=3):
-    
+    embedding = []
+
+    print(f"\nRegistering {name}...")
+    print(f"Capturing {number_of_images} face samples.")
+    print("Look directly at the camera.\n")
+
+    while len(embedding) < number_of_images:
+        success, frame = cap.read()
+
+        if not success:
+            print("Could not read Camera Frame")
+            continue
+
+        frame = cv2.flip(frame,1)
+
+        cv2.putText(
+            frame,
+            f"Registering {name}",
+            (20,40),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0,255,255),
+            2
+        )
+
+        cv2.imshow(
+            "Face Recognition",frame
+        )
